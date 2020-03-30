9f267cd942aff1cd668465a18d514deb4df7d941
Rostislav <koti4Matroskin@gmail.com>
2020-03-30 22:02:54 +0300

    listView, textEdit

--- enotherView.py
+++ enotherView.py
@@ -1,8 +1,10 @@
 import os, sys, datetime
+from PyQt5 import QtCore, QtGui
 from PyQt5.QtWidgets import (QMainWindow, QWidget, QApplication, QPushButton, QHBoxLayout,
                              QVBoxLayout, QDesktopWidget, QGroupBox, QListView,
                              QTextEdit,
                             )
+
 
 
 class MainWindow(QMainWindow):
@@ -22,7 +24,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Eventeger")
         self.statusBar().showMessage("We are ready!", 900)
 
-        self.setGeometry(310, 320, 250, 150)
+        # self.setGeometry(932, 553, 250, 150)
         self.setMinimumSize(250, 250)
         self.setMaximumSize(screenGeometry.width()//2.5, screenGeometry.height()-200)
         self.setCentralWidget(workspace)
@@ -36,26 +38,54 @@ class MainWindow(QMainWindow):
 
         textEdit = QTextEdit()
         textEdit.setMaximumHeight(44)
-        textEdit2 = QTextEdit()
-        textEdit3 = QTextEdit()
-        textEdit3.setMaximumHeight(100)
+
+        listView = QListView()
+        listView.setMinimumHeight(150)
+        self.entry = QtGui.QStandardItemModel()
+        listView.setModel(self.entry)
+        
 
         sndBtn = QPushButton("Send")
         sndBtn.setMaximumHeight(44)
+        sndBtn.clicked.connect(textEdit.append("text"))
+
+        centerLayout.addWidget(listView)
 
         footerLayout.addWidget(textEdit)
         footerLayout.addWidget(sndBtn)
 
-
-        globalVBox.addWidget(textEdit3)
-        globalVBox.addWidget(textEdit2)
+        globalVBox.addLayout(centerLayout)
         globalVBox.addLayout(footerLayout)
-        
+
         # globalVBox.addLayout(upperHBox)
         # globalVBox.addLayout(centerHBox)
         # globalVBox.addLayout(downHBox)
 
         self.show()
+
+
+
+####    Position functionality =)   #
+    def centralPosition(self):
+        print("Available:", QDesktopWidget().availableGeometry())
+        centerPoint = QDesktopWidget().availableGeometry().center()
+        print("Screen center:", centerPoint)
+        frameCenter = self.frameGeometry()
+        print("Frame center:", frameCenter)
+
+
+
+        frameCenter.moveCenter(centerPoint)
+        self.move(frameCenter.topLeft())
+        # move(frameCenter.center())
+        print("Move to center")
+#####################################
+####    Send mdg                    #
+    def sendMsg(self):
+        self.entry.appendRow("msg")
+####################################
+
+
         
     
 
